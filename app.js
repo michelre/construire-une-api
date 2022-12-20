dec7d9575fe8eff8f6fa75b1135cdca2691adf7e
mary elodie <99305063+MARYKKKK@users.noreply.github.com>
2022-12-20 23:33:22 +0100

    installation mongobdb

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 const express = require('express');
-const app = express ();
-module.exports = app ;
-app.use ((req, res)=>{
-    res.json ({'votre requêtte a bien été reçu'});
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log('Requête reçue !');
+  next();
 });
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: 'Votre requête a bien été reçue !' });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Réponse envoyée avec succès !');
+});
+
 module.exports = app;
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://mary:<energybolder>@cluster0.ht52jvv.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+});
+
+
